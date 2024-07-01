295fcc1db7aecbeb293209389d2c423e570984ee
teom142 <xodn9245@naver.com>
2024-07-01 15:07:30 +0900

    init : 초기 세팅

---
+++ proma/config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+# 서버 내에서 구동하는 명령어
+# python manage.py runserver --settings=config.settings.prod
+
+# 장고 실행시 사용하는 setting파일에 대한 환경변수
+# set DJANGO_SETTINGS_MODULE=config.settings.prod
+# python manage.py runserver
+
+# ex. ALLOWED_HOSTS = ['3.37.58.70']
