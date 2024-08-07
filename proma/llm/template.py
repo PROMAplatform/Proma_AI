da85bf6f06875c127c723eb012a24f4fb3956284
teom142 <xodn9245@naver.com>
2024-08-08 01:15:41 +0900

    modify history template

--- proma/llm/template.py
+++ proma/llm/template.py
@@ -11,9 +11,7 @@ Rule: Do not use profanity or profanity.
 """
 
 history_template = """
-This is a previous conversation between you and the user. Answer by referring to this conversation.
+This is a previous conversation between you and the user. 
+Answer by referring to this conversation.
 Never give the same answer as you gave before.
-Repeating what you said before is like a bot, so don't repeat the same thing like a human.
-Never repeat the same answer you gave before.
-If you don't have any previous conversation history, just answer.
 """
