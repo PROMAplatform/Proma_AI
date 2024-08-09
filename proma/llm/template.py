9b4c3576876a9c8377778baea338a5d941c5ed3c
teom142 <xodn9245@naver.com>
2024-08-09 15:11:40 +0900

    modify template.py

--- proma/llm/template.py
+++ proma/llm/template.py
@@ -18,4 +18,5 @@ Never give the same answer as you gave before.
 
 implicit_template = """
 Don't answer longer than 1024 characters.
+Please answer as if you were the speaker and the user was the listener.
 """
