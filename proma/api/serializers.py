ec3dd0e668dfbb5289304d37134363e24edb4e38
teom142 <xodn9245@naver.com>
2024-08-23 14:55:43 +0900

    fix api.serializers

--- proma/api/serializers.py
+++ proma/api/serializers.py
@@ -3,7 +3,7 @@ from users.models import chatroom_tb
 from llm.models import message_tb
 
 class QuestionSerializer(serializers.Serializer):
-     userLoginId = serializers.CharField(max_length=1024)
+    userLoginId = serializers.CharField(max_length=1024)
     apiToken = serializers.CharField(max_length=1024)
     secretKey = serializers.CharField(max_length=1024)
     messageQuestion = serializers.CharField(max_length=1024)
