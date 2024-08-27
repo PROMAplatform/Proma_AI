196001f5e41c7e3df6f9cf3b7a68ebebb6326e8c
teom142 <xodn9245@naver.com>
2024-08-27 22:42:36 +0900

    modify messageQuestion max_length

--- proma/llm/serializers.py
+++ proma/llm/serializers.py
@@ -4,7 +4,7 @@ from .models import message_tb
 class PromptSerializer(serializers.Serializer):
     promptId = serializers.IntegerField(allow_null=True, required=False)
     chatroomId = serializers.IntegerField(allow_null=True)
-    messageQuestion = serializers.CharField(max_length=200)
+    messageQuestion = serializers.CharField(max_length=10000)
     fileType = serializers.CharField(max_length=128, allow_blank=True)
     messageFile = serializers.CharField(max_length=512, allow_blank=True)
 
