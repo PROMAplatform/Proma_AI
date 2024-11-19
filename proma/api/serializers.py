2e20a5a73cc0c724cc1ae485b558d17a2d50e65a
teom142 <xodn9245@naver.com>
2024-11-19 21:54:20 +0900

    feat: add proma api image

--- proma/api/serializers.py
+++ proma/api/serializers.py
@@ -7,6 +7,8 @@ class QuestionSerializer(serializers.Serializer):
     apiToken = serializers.CharField(max_length=1024)
     secretKey = serializers.CharField(max_length=1024)
     messageQuestion = serializers.CharField(max_length=1024)
+    fileType = serializers.CharField(max_length=128, allow_blank=True)
+    messageFile = serializers.CharField(max_length=512, allow_blank=True)
 
 class TestSerializer(serializers.Serializer):
     promptId = serializers.IntegerField(allow_null=True, required=False)
--- proma/api/views.py
+++ proma/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import QuestionSerializer, ChatroomSerilaizer, MessageSerializer, TestSerializer
 from llm.utils import find_payload, get_history_tuple, llm_answer_his
+from llm.multimodal.image import llm_answer_his_img
 from llm.models import prompt_tb
 from users.models import user_tb, chatroom_tb
 # Create your views here.
@@ -16,6 +17,8 @@ def api_question(request):
         key = serializer.data['secretKey']
         messageQuestion = serializer.data['messageQuestion']
         userLoginId = serializer.data['userLoginId']
+        messageFile = serializer.data['messageFile']
+        fileType = serializer.data['fileType']
         payload = find_payload(token, key)
         user = user_tb.objects.get(social_id=payload['id'])
         try:
@@ -40,11 +43,14 @@ def api_question(request):
             chatroom = chatroom_serializer.save()
         prompt = prompt_tb.objects.get(pk=payload['promptId']).prompt_preview
         history = get_history_tuple(chatroom.id)
-        answer = llm_answer_his(prompt, messageQuestion, history)
+        if fileType == "image":
+            answer = llm_answer_his_img(prompt, messageQuestion, messageFile, history)
+        else:
+            answer = llm_answer_his(prompt, messageQuestion, history)
         data = {
             "prompt": payload['promptId'],
             "message_answer": answer,
-            "message_file": "",
+            "message_file": messageFile,
             "message_question": messageQuestion,
             "chatroom": chatroom.id,
         }
